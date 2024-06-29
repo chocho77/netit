f59b9bcff7f468ffc3b7c3d67ac697a904efa1af
chocho77 <chavmom@gmail.com>
2024-06-29 12:37:30 +0300

    On branch master  Your branch is up to date with 'origin/master'.  Changes to be committed: 	modified:   lecture16/project01/Project01.py 	new file:   lecture16/project01/__pycache__/validation.cpython-39.pyc 	modified:   lecture16/project01/validation.py

--- lecture16/project01/Project01.py
+++ lecture16/project01/Project01.py
@@ -1,12 +1,17 @@
 from ui import print_user_options, take_user_input
+from validation import is_main_menu_input_valid
 
 if __name__ == '__main__':
     print("Automotive inventory")
-    print_user_options()
-    user_input = take_user_input()
-    print(user_input)
+    while True:
+        print_user_options()
+        user_input = take_user_input()
+        if is_main_menu_input_valid(user_input):
+            print(f"User chose {user_input}")
+        else:
+            print("Wrong options")
+    
     # while True:
        # print_user_options
        # user_input = take_user_input()
        # map_user_input(user_input)
-
--- lecture16/project01/validation.py
+++ lecture16/project01/validation.py
@@ -1,8 +1,12 @@
 from vehicle_management import MAIN_MENU_OPTIONS
 
 
-def is_user_main_menu_input_valid(user_input: str)-> bool:
+def is_main_menu_input_valid(user_input: str)-> bool:
     if not user_input.isdigit():
         return False
-    user_input_num = int(user_input)
+    user_input_num = int(user_input) - 1
+    if user_input_num < 0 or user_input_num >= len(MAIN_MENU_OPTIONS):
+        return False
+    return True
+
     
