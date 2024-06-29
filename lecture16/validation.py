3e5e0c14f429e5693789c9627bd67d45feb836c4
chocho77 <chavmom@gmail.com>
2024-06-29 11:55:56 +0300

    On branch master  Your branch is up to date with 'origin/master'.  Changes to be committed: 	modified:   lecture16/validation.py

--- lecture16/validation.py
+++ lecture16/validation.py
@@ -1,2 +1,8 @@
-def is_user_main_menu_input_valid()-> bool:
-    pass
+from vehicle_management import MAIN_MENU_OPTIONS
+
+
+def is_user_main_menu_input_valid(user_input: str)-> bool:
+    if not user_input.isdigit():
+        return False
+    user_input_num = int(user_input)
+    
