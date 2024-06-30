925b4db28db88a85f1ee94aed5aedb2fa0306759
chocho77 <chavmom@gmail.com>
2024-06-30 13:52:20 +0300

    On branch master  Your branch is up to date with 'origin/master'.  Changes to be committed: 	new file:   lecture16/project1/project01/__pycache__/database.cpython-39.pyc 	modified:   lecture16/project1/project01/database.py 	new file:   lecture16/project1/project01/test_database.py 	deleted:    lecture16/project1/unit_test/test_database.py

--- lecture16/project1/project01/database.py
+++ lecture16/project1/project01/database.py
@@ -1,12 +1,18 @@
 db = []
 
+def get_db():
+    global db
+    
+    return db
+
+
 
 def add_vehicle_to_db(make: str,
                       model: str,
                       year: int,
                       color: str,
                       range: float):
-    db.append(make, model, year, color, range)
+    db.append((make, model, year, color, range))
 
 
 def delete_vehicle(id: int):
---
+++ lecture16/project1/project01/test_database.py
@@ -0,0 +1,13 @@
+import unittest
+from database import add_vehicle_to_db, get_db
+
+class MyTestCase(unittest.TestCase):
+    add_vehicle_to_db("BMW", "X5", 2000, "green", 300_000)
+    def test_something(self):
+        self.assertEqual(get_db(), [("BMW", "X5", 2000, "green", 300_000)])
+        
+
+if __name__ == '__main__':
+    unittest.main()
+
+
