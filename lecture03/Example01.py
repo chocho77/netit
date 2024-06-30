e80cb9d361e05a26da152a053af5aff8784a2453
chocho77 <chavmom@gmail.com>
2024-06-30 13:04:26 +0300

    On branch master  Your branch is up to date with 'origin/master'.  Changes to be committed: 	new file:   lecture03/Example01.py 	new file:   lecture03/Example02.py

---
+++ lecture03/Example01.py
@@ -0,0 +1,8 @@
+def main_func():
+    a = 3
+    print(id(a))
+
+if __name__ == '__main__':
+    main_func()
+    
+
---
+++ lecture03/Example02.py
@@ -0,0 +1,14 @@
+def main_func():
+    comment = """
+               Multi 
+               line
+               strings.          
+ 
+             """
+    print(comment)
+
+
+if __name__ == '__main__':
+    main_func()
+
+
