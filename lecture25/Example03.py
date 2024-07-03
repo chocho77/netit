bbe223952357d57e0e9cc70f9fea6227ff544cb3
chocho77 <chavmom@gmail.com>
2024-07-03 10:19:22 +0300

    On branch master  Your branch is up to date with 'origin/master'.  Changes to be committed: 	new file:   lecture25/Example03.py 	new file:   lecture25/Example04.py

---
+++ lecture25/Example03.py
@@ -0,0 +1,4 @@
+import matplotlib.pyplot as plt
+plt.plot([0,1,2,4,3],[1,5,3,4,5], "ro")
+plt.show()
+
---
+++ lecture25/Example04.py
@@ -0,0 +1,5 @@
+import matplotlib.pyplot as plt
+x = [0, 1, 2, 4, 3]
+y = [1, 5, 3, 4, 5]
+plt.plot(x, y, "go-")   # plot_format {color}{marker}{line}
+plt.show()
