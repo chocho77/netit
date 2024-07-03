6cf762b7aded4e5ba5bf76a36862c49130ae6e06
chocho77 <chavmom@gmail.com>
2024-07-03 11:15:39 +0300

    On branch master  Your branch is up to date with 'origin/master'.  Changes to be committed: 	modified:   lecture25/Example05.py

--- lecture25/Example05.py
+++ lecture25/Example05.py
@@ -6,6 +6,10 @@ x1 = [4, 6, 7, 8]
 y1 = [2, 4, 6, 1]
 
 fig, (ax1, ax2) = plt.subplots(1, 2)
+ax1.set_xlabel("Hello")
+ax2.set_ylabel("Hello again")
+ax1.set_xlim(-5,  10)
+plt.tight_layout()
 ax1.plot(x, y)
 ax2.plot(x1, y1)
 plt.show()
