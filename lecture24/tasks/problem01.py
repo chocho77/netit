bccc9ee1eb350dff768e85306c4899100c35c416
chocho77 <chavmom@gmail.com>
2024-07-04 21:43:39 +0300

    On branch master  Your branch is up to date with 'origin/master'.  Changes to be committed: 	modified:   lecture24/tasks/problem01.py

--- lecture24/tasks/problem01.py
+++ lecture24/tasks/problem01.py
@@ -2,6 +2,13 @@ from datetime import datetime
 
 def main_func():
     print(datetime.now())
+    print(datetime.now().year)
+    print(datetime.now().month)
+    print(datetime.now().strftime("%W"))
+    print(datetime.now().weekday())
+    print(datetime.now().strftime("%j"))    # for more code: https://strftime.org
+    print(datetime.now().strftime("%d"))    # for more info: https://strftime.org
+    print(datetime.now().strftime("%A"))    # for more info: https://strftime.org
     pass
 
 if __name__ == '__main__':
