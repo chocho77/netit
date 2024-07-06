d823f0b5a88aafe0032a50b1443956c3e99d42a1
chocho77 <chavmom@gmail.com>
2024-07-06 16:09:11 +0300

    On branch master  Your branch is up to date with 'origin/master'.  Changes to be committed: 	modified:   lecture32/example03.py

--- lecture32/example03.py
+++ lecture32/example03.py
@@ -1,1 +1,28 @@
+import asyncio
+import time
+
 # https://www.slingacademy.com/article/python-3-11-asyncio-streamwriter-a-practical-guide-with-examples/
+
+async def count():
+    await asyncio.sleep(1)
+    print('1')
+    await asyncio.sleep(1)
+    print('2')
+    await asyncio.sleep(1)
+    print('3')
+
+async def main():
+    tasks = []
+    for i in range(200):
+        task = asyncio.create_task(count())
+        tasks.append(task)
+    
+    for task in tasks:
+        await task
+
+if __name__ == '__main__':
+    start_time = time.perf_counter()
+    asyncio.run(main())
+    end_time = time.perf_counter()
+    total_time = end_time - start_time
+    print(f'Total time : {total_time:0.2f} seconds.')
