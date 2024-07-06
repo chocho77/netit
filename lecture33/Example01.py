5cf2f1abddc3e45171b444778c4067d8006c170d
chocho77 <chavmom@gmail.com>
2024-07-06 14:03:13 +0300

    On branch master  Your branch is up to date with 'origin/master'.  Changes to be committed: 	modified:   lecture33/Example01.py

--- lecture33/Example01.py
+++ lecture33/Example01.py
@@ -4,3 +4,4 @@
 # https://superfastpython.com/threadpoolexecutor-map/
 # https://ipython.ai/practical-threading-python-guide/
 # https://superfastpython.com/python-asynchronous-programming/
+# https://superfastpython.com/multiprocessing-in-python/
