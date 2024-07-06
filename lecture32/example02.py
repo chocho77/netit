6b6f81ab49341b2df1c89bfe147ab6dd6c306f1a
chocho77 <chavmom@gmail.com>
2024-07-06 09:27:21 +0300

    On branch master  Your branch is up to date with 'origin/master'.  Changes to be committed: 	new file:   lecture32/example02.py

---
+++ lecture32/example02.py
@@ -0,0 +1,33 @@
+import concurrent.futures
+import time
+
+def do_something(*args, **kwargs):
+    raise Exception('Something bad happend')
+    return 'String'
+
+if __name__ == '__main__':
+    start = time.perf_counter()
+
+    threads_count = 20
+    with concurrent.futures.ThreadPoolExecutor(max_workers=threads_count) as executor:
+        number_of_args = 4
+        secs = [i for i in range(number_of_args)]
+
+        futures = []
+        future =executor.submit(do_something, secs)
+        futures.append(future)
+
+        for future in futures:
+            exception_rised = future.exception()
+            if exception_rised:
+                print(f'Exception was raised: {exception_rised}')
+            
+            try:
+                result = future.result()
+            except Exception as exception:
+                result = f'Gave up on result. Error: {exception}'
+
+            print(result)
+                
+
+            print(future.result())
