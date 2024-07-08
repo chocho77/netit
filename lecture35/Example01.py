7dad4d4f5649e08fe02e2e7ddb4ebe5913cacb63
chocho77 <chavmom@gmail.com>
2024-07-08 13:26:44 +0300

    On branch master  Your branch is up to date with 'origin/master'.  Changes to be committed: 	new file:   lecture35/Example01.py 	new file:   lecture35/Example02.py 	new file:   lecture35/Example03.py 	new file:   lecture35/Example04.py

---
+++ lecture35/Example01.py
@@ -0,0 +1,7 @@
+import multiprocessing
+
+# learn source: 
+# https://www.digitalocean.com/community/tutorials/python-multiprocessing-example
+
+
+print("Number of cpu : ", multiprocessing.cpu_count())
