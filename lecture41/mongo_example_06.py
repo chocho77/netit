ee2343898d6b3975c860808737d3e1c8d7b3c5e9
chocho77 <chavmom@gmail.com>
2024-07-23 07:58:46 +0300

    On branch master  Your branch is up to date with 'origin/master'.  Changes to be committed: 	new file:   lecture41/mongo_example_06.py

---
+++ lecture41/mongo_example_06.py
@@ -0,0 +1,14 @@
+import pymongo
+
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = myclient["mydatabase"]
+mycol = mydb["customers"]
+
+myquery = { "address": "Park Lane 38" }
+
+mydoc = mycol.find(myquery)
+
+for x in mydoc:
+  print(x)
+
+  # more info : https://www.w3schools.com/python/python_mongodb_query.asp
